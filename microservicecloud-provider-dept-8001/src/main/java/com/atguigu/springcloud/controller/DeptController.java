80c6abceacdc855e9237024f431ad820c5223be3
zhangmn <Zhang1880!>
2019-11-22 10:37:47 +0800

    服务发现Discovery

--- microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/controller/DeptController.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/controller/DeptController.java
@@ -3,6 +3,8 @@ package com.atguigu.springcloud.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import com.atguigu.springcloud.entities.Dept;
@@ -13,6 +15,8 @@ public class DeptController
 {
     @Autowired
     private DeptService service;
+    @Autowired
+    private DiscoveryClient client;
 
     @GetMapping(value="/dept/getadd/{dname}")
     public boolean getAddPathVariable(@PathVariable("dname") String dname)
@@ -42,6 +46,20 @@ public class DeptController
     public List<Dept> list()
     {
         return service.list();
+    }
+
+    @RequestMapping(value = "/dept/discovery", method = RequestMethod.GET)
+    public Object discovery()
+    {
+        List<String> list = client.getServices();
+        System.out.println("**********" + list);
+
+        List<ServiceInstance> srvList = client.getInstances("MICROSERVICECLOUD-DEPT");
+        for (ServiceInstance element : srvList) {
+            System.out.println(element.getServiceId() + "\t" + element.getHost() + "\t" + element.getPort() + "\t"
+                    + element.getUri());
+        }
+        return this.client;
     }
 
 
