0d06d995ea7002a3240f8b0364f93b960ccf27c4
zhangmn <Zhang1880!>
2019-11-22 10:19:57 +0800

    将8001注册到7001中

--- microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/DeptProvider8001_App.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/DeptProvider8001_App.java
@@ -2,8 +2,10 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient //本服务启动后会自动注册进eureka服务中
 public class DeptProvider8001_App
 {
     public static void main(String[] args)
@@ -11,5 +13,6 @@ public class DeptProvider8001_App
         SpringApplication.run(DeptProvider8001_App.class, args);
     }
 }
+
 
 
